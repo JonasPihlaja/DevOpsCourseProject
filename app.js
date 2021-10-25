dd3db5d58e0ffc1bc9e70fdbf526ebc9855390ee
Jonas Pihlaja <jonas.pihlaja@hotmail.com>
2021-10-25 14:27:47 +0300

    v0.2

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const { App } = require("@slack/bolt");
+const {Webclient} = require("@slack/web-api")
 require("dotenv").config();
+const receiver = require('./receiver');
+const listener = receiver.listener;
+const getNotifications = receiver.getNotification;
+const list = receiver.list;
+
+
+let notificationArr;
+listener.use((req, resp, next) => {
+  notificationArr = list;
+  console.log("getNotifications")
+  console.log(list)
+  next();
+});
+listener.listen(3030, () => console.log(`Receiver listening on port 3030`));
+
+
+
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -10,5 +29,30 @@ const app = new App({
   const port = 3000
   // Start your app
   await app.start(process.env.PORT || port);
+  //app.action()
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+
 })();
+
+
+/*const express = require('express');
+const serv = express();
+const PORT = process.env.PORT || 3030;
+
+serv.use(express.json());
+serv.post('/notifications', (req, res) => res.json(req.body));
+serv.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+receiver.router.post("/notifications", (_req, res) => {
+  console.log(_req);
+  res.send("You can access this page without x-slack- headers!");
+});
+
+receiver.router.get("/", (_req, res) => {
+  res.send("You can access this page without x-slack- headers!");
+});
+*/
--- receiver.js
+++ receiver.js
@@ -1,9 +1,98 @@
 const express = require('express');
-const app = express();
+const receiver = express();
 const PORT = process.env.PORT || 3030;
+const bodyParser = require('body-parser');
+const request = require('request')
 
-app.use(express.json());
+let notificationsArr = [];
 
-app.post('/test', (req, res) => res.json(req.body));
+receiver.use(bodyParser.urlencoded({ extended: false }))
+receiver.use(bodyParser.json())
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+receiver.use(express.json());
+receiver.post('/notifications', (req, res) => {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    });
+
+    req.on('end', () => {
+        let payload = JSON.parse(body)
+        //console.log(payload)
+        setNotification(payload)
+    });
+    return res.end("ok");
+});
+
+function setNotification(payload){
+    let notification = {};
+    notification.id = payload.MessageId;
+    notification.message = payload.Message;
+    if(payload.Subject){
+        notification.subject = payload.Subject;
+    }
+    if(payload.MessageAttributes){
+        notification.attributes = payload.MessageAttributes;
+    }
+    let idUnique = true;
+    if(notificationsArr.length > 0){
+        for(let i = 0; i>notificationsArr.length; i++){
+            if (notificationsArr[i].id == notification.id) {
+                idUnique == false;
+            } 
+        }
+        if(idUnique == true){
+            notificationsArr.push(notification);
+        }
+    } else{
+        notificationsArr.push(notification);
+    }
+    console.log(notificationsArr);
+}
+
+function getNotification(){
+    return notificationsArr;
+}
+module.exports.list = notificationsArr;
+module.exports.getNotification = getNotification;
+module.exports.listener = receiver;
+
+
+/*
+THIS WAS USED TO ACCEPT A SUBSCRIPTION FOR AN AWS NOTIFICATION SERVICE
+receiver.use(bodyParser.urlencoded({ extended: false }))
+receiver.use(bodyParser.json())
+
+receiver.post('/notifications', (req, res) => {
+    console.log(req.body)
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+  
+    req.on('end', () => {
+      let payload = JSON.parse(body)
+  
+      if (payload.Type === 'SubscriptionConfirmation') {
+        const promise = new Promise((resolve, reject) => {
+          const url = payload.SubscribeURL
+  
+          request(url, (error, response) => {
+            if (!error && response.statusCode == 200) {
+              console.log('Yess! We have accepted the confirmation from AWS')
+              return resolve()
+            } else {
+              return reject()
+            }
+          })
+        })
+  
+        promise.then(() => {
+          res.end("ok")
+        })
+      }
+    })
+  });
+*/
